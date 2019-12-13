b3ce668fd27e87e685f39086104582447838bae2
unknown <gabriell.is.huver@gmail.com>
2019-12-13 15:06:34 -0300

    fixes

--- PyRastreamentoCorreios/RastreamentoAPI.py
+++ PyRastreamentoCorreios/RastreamentoAPI.py
@@ -6,49 +6,47 @@ BASEURL = "https://www.linkcorreios.com.br/"
 
 
 def rastrear(codigo):
-    page = requests.get(BASEURL + codigo)
-    soup = BeautifulSoup(page.text, "html.parser")
-    tdList = soup.find_all("td")
-    statusList = []
-    for i in range(len(tdList)):
-        try:
-            time.strptime(tdList[i].getText(), "%d/%m/%Y %H:%S")
-            data = tdList[i].getText()
-            status = tdList[i + 1].getText()
+        page = requests.get(BASEURL + codigo)
+        soup = BeautifulSoup(page.text, "html.parser")
+        tdList = soup.find_all("td")
+        statusList = []
+        for i in range(len(tdList)):
             try:
-                if "Local:" in tdList[i + 2].getText():
-                    local = tdList[i + 2].getText()
-                else:
+                time.strptime(tdList[i].getText(), "%d/%m/%Y %H:%S")
+                data = tdList[i].getText()
+                status = tdList[i + 1].getText()
+                try:
+                    if "Local:" in tdList[i + 2].getText():
+                        local = tdList[i + 2].getText()
+                    else:
+                        local = ""
+                except:
                     local = ""
-            except:
-                local = ""
-            try:
-                if "Origem:" in tdList[i + 2].getText():
-                    origem = tdList[i + 2].getText()
-                else:
+                try:
+                    if "Origem:" in tdList[i + 2].getText():
+                        origem = tdList[i + 2].getText()
+                    else:
+                        origem = ""
+                except:
                     origem = ""
-            except:
-                origem = ""
-            try:
-                if "Destino:" in tdList[i + 3].getText():
-                    destino = tdList[i + 3].getText()
-                else:
+                try:
+                    if "Destino:" in tdList[i + 3].getText():
+                        destino = tdList[i + 3].getText()
+                    else:
+                        destino = ""
+                except:
                     destino = ""
+                statusList.append(
+                    {
+                        "data": data,
+                        "status": status,
+                        "local": local,
+                        "origem": origem,
+                        "destino": destino,
+                    }
+                )
             except:
-                destino = ""
-            statusList.append(
-                {
-                    "data": data,
-                    "status": status,
-                    "local": local,
-                    "origem": origem,
-                    "destino": destino,
-                }
-            )
-        except:
-            pass
-    return reversed(statusList)
-
-
-
+                pass
+        return reversed(statusList)
+    
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'PyRastreamentoCorreios',        
   packages = ['PyRastreamentoCorreios'],   
-  version = '0.1.2',      
+  version = '0.1',      
   license='MIT',        
   description = 'Biblioteca de rastreamento dos correios',   
   author = 'Gabriell Huver',                   
@@ -12,18 +12,13 @@ setup(
   keywords = ['CORREIOS', 'RASTREAMENTO', 'LINKCORREIOS'],   
   install_requires=[           
           'requests',
-          'beautifulsoup4',
-          'time'
+          'beautifulsoup4'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.7',
   ],
 )
