fa230ef4287b2c88bdd54e7282377a79a75a32f1
Gabriell Huver <gabriell.is.huver@gmail.com>
2020-02-26 11:36:53 -0300

    fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'PyRastreamentoCorreios',        
   packages = ['PyRastreamentoCorreios'],   
-  version = '0.2',      
+  version = '0.2.1',      
   license='MIT',        
   description = 'Biblioteca de rastreamento dos correios',   
   author = 'Gabriell Huver',                   
